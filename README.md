0d8f4a7ec7e6af5bf4313de0f7571a4ee5f00b92
nangell18 <65080006+nangell18@users.noreply.github.com>
2021-09-13 18:17:14 -0400

    this is for the second assignment

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Math 421. Statistical analysis with R
 
 [Click here to my Assignment 1](Assignment1.html)
+[Click here to my Assignment 1](Assignment2.html)
---
+++ assignment 2.R
@@ -0,0 +1,22 @@
+# problem 1
+sum(seq(1,100,3))
+
+#problem 2
+x <- c(1:2020)
+sum(x^2020)
+
+# problem 3
+x <- c(1:2020)
+y <- c(2:2021)
+sum(1/(x*y))
+
+# problem 4
+x <- c(1:2020)
+y <- c(2:2021)
+z <- c(3:2022)
+sum(1/(x*y*z))
+
+# problem 5
+x <- seq(1,2021,2)
+y <- seq(1,2021,2)
+sum(x^y)
